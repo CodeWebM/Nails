0c1796c56cf2c0b125a37203a4db293c76f913d5
CodeWebM <100290211+CodeWebM@users.noreply.github.com>
2022-02-25 14:43:28 -0300

    Add files via upload
    initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+window.sr = ScrollReveal();
+
+  sr.reveal('.home-img', {
+      duration: 3000, 
+      origin: 'bottom',
+      distance: '-100px'
+
+  });
+
+  sr.reveal('.home-text', {
+    duration: 3000, 
+    origin: 'left',
+    distance: '400px'
+
+});
+
+sr.reveal('.home-subtitle', {
+    duration: 3000, 
+    origin: 'bottom',
+    distance: '400px'
+
+});
+
